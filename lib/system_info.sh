22f170a69901d3742a92b6b3247465c67d35e8d3
xydarknet <119782134+xydarknet@users.noreply.github.com>
2025-07-09 22:23:51 +0700

    Update system_info.sh

--- lib/system_info.sh
+++ lib/system_info.sh
@@ -1,1 +1,8 @@
+#!/bin/bash
 
+OS_INFO=$(grep PRETTY_NAME /etc/os-release | cut -d= -f2 | tr -d '"')
+KERNEL=$(uname -r)
+CPU=$(lscpu | grep 'Model name' | awk -F: '{print $2}' | sed -e 's/^[ \t]*//')
+RAM=$(free -h | awk '/Mem:/ {print $2 " Total / " $3 " Used"}')
+IP=$(curl -s ifconfig.me)
+REGION=$(curl -s ipinfo.io/country)
